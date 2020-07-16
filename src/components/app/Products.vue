53f533d183e5fc7e951bc259a45bc807d1f1fa02
Kostya <Kpstya@mail.ru>
2020-07-17 00:10:50 +0300

    this variant is work

--- src/components/app/Products.vue
+++ src/components/app/Products.vue
@@ -22,7 +22,6 @@
                 catalogUrl: '/catalogData.json',
                 products: [],
                 filtered: [],
-
                 api: 'https://raw.githubusercontent.com/Konstantin108/new_variant_s-project2/last_try/responses',
                 userSearch: '',
             }
